9b0c6d96a614f78d2c112c34bf3726f1a6cf3229
thomasmaltese <thomasmaltese64@gmail.com>
2018-02-18 11:25:49 -0500

    monitor command for watching files

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 # system files
 cp mongodb.service-ulimit.hook /usr/share/libalpm/hooks/mongodb.service-ulimit.hook
 
+# user files
 BINDIR=/home/tom/bin
 USRFILES="
 mongodb.service-ulimit.py
 arch-mirrors.sh
+monitor
 "
 
 for f in $USRFILES; do
