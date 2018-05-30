77717e35e82ad32200358db319f19e0c34364509
thomasmaltese <thomasmaltese64@gmail.com>
2018-05-29 20:47:37 -0400

    install.sh with sys dir and ls -I

--- install.sh
+++ install.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# system files
-cp mongodb.service-ulimit.hook /usr/share/libalpm/hooks/mongodb.service-ulimit.hook
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+	echo "Usage: `basename $0` <sys>"
+	echo "Install files to \$HOME/bin"
+	echo "If sys is specified, install sys files"
+	exit 1;
+fi
+
+if [ "$1" = "sys" ]; then
+	sudo cp sys/mongodb.service-ulimit.hook /usr/share/libalpm/hooks/mongodb.service-ulimit.hook
+	exit 0
+fi
+## else
 
 # user files
 BINDIR=/home/tom/bin
-USRFILES="
-mongodb.service-ulimit.py
-arch-mirrors.sh
-monitor
-i3-set-background
-youtube-playlist
-"
-
-for f in $USRFILES; do
+ignore=`cat ignore`
+cmd="ls"
+for i in $ignore; do
+	cmd="$cmd -I $i"
+done
+files=`eval "$cmd"`
+for f in $files; do
 	cp $f "$BINDIR/$f"
 done
-
-chown -R tom:tom "$BINDIR"
+exit 0
